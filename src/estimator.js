4d42a680d8449321c373bab51e69f6db0f08db83
Said Jillo <umurjillo@gmail.com>
2020-04-11 22:47:33 +0300

    removed trailing spaces

--- src/estimator.js
+++ src/estimator.js
@@ -1,10 +1,8 @@
 const covid19ImpactEstimator = (data) => data;
 
-const covid19DataReport = 
-    (regionData, periodType, timeToElapse, reportedCases, population, totalHospitalBeds) => {
- 
+const covid19DataReport = (regionData, periodType, timeToElapse, 
+    reportedCases, population, totalHospitalBeds) => {
     const resultantReport = {};
-
     resultantReport.data = {
         region: {
             name: regionData.region,
@@ -18,38 +16,29 @@ const covid19DataReport =
         population: population,
         totalHospitalBeds: totalHospitalBeds
     };
-
     resultantReport.impact = {
         currentlyInfected: reportedCases * 10,
         infectionsByRequestedTime: currentlyInfected * 1024
     };
-
     resultantReport.severeImpact = {
         currentlyInfected: reportedCases * 50,
         infectionsByRequestedTime: currentlyInfected * 1024
     };
-
     console.log(resultantReport);
     return covid19ImpactEstimator(resultantReport);
-
 }
 
 const covid19CaptureFunction = 
     (name,avgAge, avgDailyIncomeInUSD,
         avgDailyIncomePopulation,periodType, timeToElapse, reportedCases,
-        population, totalHospitalBeds) => {
-    
+        population, totalHospitalBeds) => {   
     const regionData = {
         name: name,
         avgAge: avgAge,
         avgDailyIncomeInUSD: avgDailyIncomeInUSD,
         avgDailyIncomePopulation: avgDailyIncomePopulation
     };
-
-
     return covid19DataReport(regionData, periodType, timeToElapse, 
         reportedCases, population, totalHospitalBeds);
 }
-
-
 export default covid19ImpactEstimator;
