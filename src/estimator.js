a83b2e050e582850402d98523f4cdd85723f1471
Said Jillo <umurjillo@gmail.com>
2020-04-11 22:30:25 +0300

    added 2 functions covid19DataReport and covid19CaptureFunction

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,54 @@
 const covid19ImpactEstimator = (data) => data;
 
+
+
+const covid19DataReport = (regionData, periodType,timeToElapse,reportedCases, population,totalHospitalBeds) => {
+ 
+    const resultantReport = {};
+
+    resultantReport.data = {
+        region: {
+            name: regionData.region,
+            avgAge: regionData.avgAge,
+            avgDailyIncomeInUSD: regionData.avgDailyIncomeInUSD,
+            avgDailyIncomePopulation: regionData.avgDailyIncomePopulation
+            },
+        periodType: periodType,
+        timeToElapse: timeToElapse,
+        reportedCases: reportedCases,
+        population: population,
+        totalHospitalBeds: totalHospitalBeds
+    };
+
+    resultantReport.impact = {
+        currentlyInfected: reportedCases * 10,
+        infectionsByRequestedTime: currentlyInfected * 1024
+    };
+
+    resultantReport.severeImpact = {
+        currentlyInfected: reportedCases * 50,
+        infectionsByRequestedTime: currentlyInfected * 1024
+    };
+
+    console.log(resultantReport);
+    return covid19ImpactEstimator(resultantReport);
+
+}
+
+const covid19CaptureFunction = (name,avgAge,avgDailyIncomeInUSD,avgDailyIncomePopulation,periodType,timeToElapse,reportedCases,population,totalHospitalBeds) => {
+    
+    const regionData = {
+        name: name,
+        avgAge: avgAge,
+        avgDailyIncomeInUSD: avgDailyIncomeInUSD,
+        avgDailyIncomePopulation: avgDailyIncomePopulation
+    };
+
+
+    return covid19DataReport(regionData, periodType,timeToElapse,reportedCases, population,totalHospitalBeds);
+}
+
+
+covid19CaptureFunction("Africa",19.7,5,0.71,"days",58,674, 66622705,1380614);
+
 export default covid19ImpactEstimator;
