196c9c433669ad81984df96af9e0b088038fc38f
Said Jillo <umurjillo@gmail.com>
2020-04-11 23:04:08 +0300

    removed some eslint errors

--- src/estimator.js
+++ src/estimator.js
@@ -1,44 +1,43 @@
 const covid19ImpactEstimator = (data) => data;
 
 const covid19DataReport = (regionData, periodType, timeToElapse, 
-    reportedCases, population, totalHospitalBeds) => {
-    const resultantReport = {};
-    resultantReport.data = {
-        region: {
-            name: regionData.region,
-            avgAge: regionData.avgAge,
-            avgDailyIncomeInUSD: regionData.avgDailyIncomeInUSD,
-            avgDailyIncomePopulation: regionData.avgDailyIncomePopulation
-            },
-        periodType: periodType,
-        timeToElapse: timeToElapse,
-        reportedCases: reportedCases,
-        population: population,
-        totalHospitalBeds: totalHospitalBeds
-    };
-    resultantReport.impact = {
-    currentlyInfected: reportedCases * 10,
-        infectionsByRequestedTime: currentlyInfected * 1024
-    };
-    resultantReport.severeImpact = {
-        currentlyInfected: reportedCases * 50,
-        infectionsByRequestedTime: currentlyInfected * 1024
-    };
-    console.log(resultantReport);
-    return covid19ImpactEstimator(resultantReport);
-}
+  reportedCases, population, totalHospitalBeds) => {
+  const resultantReport = {};
+  resultantReport.data = {
+    region: {
+        name: regionData.region,
+        avgAge: regionData.avgAge,
+        avgDailyIncomeInUSD: regionData.avgDailyIncomeInUSD,
+        avgDailyIncomePopulation: regionData.avgDailyIncomePopulation
+        },
+    periodType: periodType,
+    timeToElapse: timeToElapse,
+    reportedCases: reportedCases,
+    population: population,
+    totalHospitalBeds: totalHospitalBeds
+  };
+  resultantReport.impact = {
+  currentlyInfected: reportedCases * 10,
+    infectionsByRequestedTime: currentlyInfected * 1024
+  };
+  resultantReport.severeImpact = {
+    currentlyInfected: reportedCases * 50,
+    infectionsByRequestedTime: currentlyInfected * 1024
+  };
+  return covid19ImpactEstimator(resultantReport);
+};
 
 const covid19CaptureFunction = 
-    (name,avgAge, avgDailyIncomeInUSD,
+  (name,avgAge, avgDailyIncomeInUSD,
     avgDailyIncomePopulation,periodType, timeToElapse, reportedCases,
     population, totalHospitalBeds) => {   
-    const regionData = {
+  const regionData = {
         name: name,
         avgAge: avgAge,
         avgDailyIncomeInUSD: avgDailyIncomeInUSD,
         avgDailyIncomePopulation: avgDailyIncomePopulation
-    };
-    return covid19DataReport(regionData, periodType, timeToElapse, 
-        reportedCases, population, totalHospitalBeds);
-}
+  };
+  return covid19DataReport(regionData, periodType, timeToElapse, 
+    reportedCases, population, totalHospitalBeds);
+};
 export default covid19ImpactEstimator;
