de0ef9bfda62fa1516de482b08669f89ea4133d7
Said Jillo <umurjillo@gmail.com>
2020-04-12 09:49:32 +0300

    removed eslint errors

--- src/estimator.js
+++ src/estimator.js
@@ -1,9 +1,10 @@
+
 const covid19ImpactEstimator = (data) => data;
 
 const covid19DataReport = (regionData, periodType, timeToElapse,
   reportedCases, population, totalHospitalBeds) => {
-  const resultantReport = {
-    data = {
+  const resultantReport = {};
+  resultantReport.data = {
     region: {
       name: regionData.region,
       avgAge: regionData.avgAge,
@@ -15,32 +16,30 @@ const covid19DataReport = (regionData, periodType, timeToElapse,
     reportedCases: reportedCases,
     population: population,
     totalHospitalBeds: totalHospitalBeds
-  },
-  impact: {
+  };
+  resultantReport.impact = {
   currentlyInfected: reportedCases * 10,
     infectionsByRequestedTime: currentlyInfected * 1024
-  },
-  severeImpact: {
+  };
+  resultantReport.severeImpact = {
     currentlyInfected: reportedCases * 50,
     infectionsByRequestedTime: currentlyInfected * 1024
-  }
-};
-
+  };
   return covid19ImpactEstimator(resultantReport);
-}; 
-
+};
 const covid19CaptureFunction = (name, avgAge, avgDailyIncomeInUSD,
   avgDailyIncomePopulation, periodType, timeToElapse, reportedCases,
   population, totalHospitalBeds) => {
   const regionData = {
-    name: name,
-    avgAge: avgAge,
-    avgDailyIncomeInUSD: avgDailyIncomeInUSD,
-    avgDailyIncomePopulation: avgDailyIncomePopulation
+    name,
+    avgAge,
+    avgDailyIncomeInUSD,
+    avgDailyIncomePopulation
   };
   return covid19DataReport(regionData, periodType, timeToElapse, 
     reportedCases, population, totalHospitalBeds);
 };
 
-covid19CaptureFunction("Africa", 19.7, 5, 0.71, "days", 58, 674, 66622705, 1380614);
+covid19CaptureFunction('Africa', 19.7, 5, 0.71, 'days', 58, 674, 66622705, 1380614);
+
 export default covid19ImpactEstimator;
